f8cbcbd1598655909f7d904df59283520e0bbe6c
emach94 <emmyajik@yahoo.com>
2016-08-18 14:11:26 +0100

    Add files via upload

---
+++ LuckySeven.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lucky.seven;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Emmy Ajik <emmyajik@yahoo.com>
+ */
+public class LuckySeven {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       lucky seven = new lucky();
+       seven.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       seven.setVisible(true);
+       seven.setLocationRelativeTo(null);
+    }
+    
+}
